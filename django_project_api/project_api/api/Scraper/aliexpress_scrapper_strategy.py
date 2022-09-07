27ba2025d2d379ea60a38cbfb9e51313e129a3d9
Dulco (Juan Ortiz) <30907887+Dulco@users.noreply.github.com>
2022-09-07 15:04:55 -0500

    Final_Aliexpress

---
+++ django_project_api/project_api/api/Scraper/aliexpress_scrapper_strategy.py
@@ -0,0 +1,47 @@
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from scraper_strategy import ScraperStrategy
+
+class AliexpressScraperStrategy(ScraperStrategy):
+
+    def __init__(self) -> None:
+            self.url=''
+
+    def empty_url(self):
+        if(self.url):
+            return False
+        else:
+            True
+
+    def get_url(self, item):
+        splitted = item.split()
+        if (len(splitted) == 1):
+            url = "https://es.aliexpress.com/af/"+item+".html?d=y&origin=n&SearchText=" + \
+                item+"&catId=0&spm=a2g0o.productlist.1000002.0&initiative_id=SB_20220830102312"
+        elif (len(splitted) > 1):
+            link1 = ''
+            link2 = ''
+            minus = 1
+            for i in splitted:
+                if minus < len(splitted):
+                    link1 = link1+i+'-'
+                    link2 = link2+i+'+'
+                else:
+                    link1 = link1+i
+                    link2 = link2+i
+            minus += 1
+            url = "https://es.aliexpress.com/af/"+link1+".html?d=y&origin=n&SearchText=" + \
+                link2+"&catId=0&spm=a2g0o.productlist.1000002.0&initiative_id=SB_20220830102312"
+        return url
+
+    def read_information(self, item):
+        if(item==''):
+            raise ValueError(f"Invalid item: Please digit a valid item")
+        else: 
+            url=self.get_url(item)
+            option = Options()
+            option.headless = True
+            driver = webdriver.Chrome(options=option)
+            driver.get(url)
+            return driver.page_source
