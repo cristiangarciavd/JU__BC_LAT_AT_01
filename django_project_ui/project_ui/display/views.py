08c9b815b0d59bbb278d29ca71b7985bddbc6d00
Cristian Garcia <cristian.garcia.vd@gmail.com>
2022-09-08 19:38:33 -0300

    UI_logger

--- django_project_ui/project_ui/display/views.py
+++ django_project_ui/project_ui/display/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 import requests
+from logger.logger import *
 
 TEMPLATE_DIRS = (
     'os.path.join(BASE_DIR, "templates"),' 
 )
 
 class Receiver:
-    
+    @wrap(entering, exiting)
     def get_products(request): 
-        
+        """ Retrieve products from API """
         response = requests.get('https://6316505533e540a6d391f345.mockapi.io/api/v1/products').json()
         return render(request, 'products.html',{'response':response})
