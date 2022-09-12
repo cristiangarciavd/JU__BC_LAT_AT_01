49c79d157c2029ed1e9345c27ca282f5b9e6ba8a
Lucas Sanabria <63239425+Lsege@users.noreply.github.com>
2022-09-12 15:48:11 -0300

    added empty db_manager

--- django_project_api/project_api/api/models.py
+++ django_project_api/project_api/api/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 class Search(models.Model):
     name = models.CharField(max_length=40) # This is the input from the user, EX: 'mouse gamer'
-    #last_search = models.DateTimeField(True, True, editable=False) # I think it has to be editable
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
