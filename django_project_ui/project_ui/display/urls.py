deb224682d8d239792431f837eb25f2c76ef1756
Dulco (Juan Ortiz) <30907887+Dulco@users.noreply.github.com>
2022-09-14 10:22:09 -0500

    Add files via upload

--- django_project_ui/project_ui/display/urls.py
+++ django_project_ui/project_ui/display/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path('',views.Receiver.get_products, name = 'products')
+     path('search',views.Receiver.get_products, name = 'products'),
+     path('home', views.index, name='home')
 ]
--- django_project_ui/project_ui/display/views.py
+++ django_project_ui/project_ui/display/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 import requests
-from logger.logger import *
 
 TEMPLATE_DIRS = (
     'os.path.join(BASE_DIR, "templates"),' 
 )
+ 
+def index(request):
+        return render(request, 'home.html')
 
 class Receiver:
-    
-    @wrap(entering, exiting)
     def get_products(request): 
-        """ Retrieve products from API """
         response = requests.get('https://6316505533e540a6d391f345.mockapi.io/api/v1/products').json()
         return render(request, 'products.html',{'response':response})
