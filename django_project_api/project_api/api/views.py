3b882c1df026181597233fbc90fe61cbd741a665
Luis bastidas <luis.basmar20@gmail.com>
2022-09-14 12:44:59 -0500

    otros cambios

--- django_project_api/project_api/api/views.py
+++ django_project_api/project_api/api/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from django.views import View
+<<<<<<< Updated upstream
 from .models import Product, Search
 from datetime import date
+=======
+from .db_manager.db_manager import DbManager
+from django.http import JsonResponse
+>>>>>>> Stashed changes
 # Create your views here. 
+from django.views import View
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 
+<<<<<<< Updated upstream
 
 
 def get_products_by_search(request, search_term):
@@ -47,3 +57,56 @@ def get_most_popular():
 		list_products = get_products_by_search(search)
 		list_of_popular_products.append(list_products)
 	return list_of_popular_products
+=======
+products_mesa_mock = [
+	{"name":"mesa marron","price":3,"image_url":"sdsds","url":"sadasd"},
+	{"name":"mesa azul","price":3,"image_url":"sdsds","url":"sadasd"},
+	{"name":"mesa amarilla","price":3,"image_url":"sdsds","url":"sadasd"}
+	]
+
+products_mouse_mock = [
+	{"name":"mouse marron","price":3,"image_url":"sdsds","url":"sadasd"},
+	{"name":"mouse azul","price":3,"image_url":"sdsds","url":"sadasd"},
+	{"name":"mouse amarilla","price":3,"image_url":"sdsds","url":"sadasd"}
+	]
+
+def parser(item):
+	if item == 'mouse':
+		product = products_mouse_mock
+	else:
+		product = products_mesa_mock
+	return product
+
+
+class CRUDView(View):
+
+	###Find Products by Search Term
+	def get(self, request, search_term):
+		db_manager = DbManager()
+		search = db_manager.find_term(search_term)
+		if search:
+			search = db_manager.term_up_today(search)
+			if search:
+				data = db_manager.find_products(search)
+			else:
+				db_manager.delete_search(search_term)
+				db_manager.create(parser(search_term),search_term)
+		else:
+			db_manager.create(parser(search_term),search_term)
+			data = parser(search_term)
+		return JsonResponse(data, safe = False)
+		
+
+	####Delete search term and products
+	# def get(self, request, search_term):
+	# 	db_manager = DbManager()
+	# 	data = db_manager.delete_search(search_term)
+	# 	return JsonResponse(data)
+
+
+	####Create Products and Search Term
+	# def get(self, request, search_term):
+	# 	db_manager = DbManager()
+	# 	
+	# 	db_manager.create([{"name":"mesa marron","price":3,"image_url":"sdsds","url":"sadasd"},{"name":"mesa azul","price":3,"image_url":"sdsds","url":"sadasd"},{"name":"mesa amarilla","price":3,"image_url":"sdsds","url":"sadasd"}], search_term)
+>>>>>>> Stashed changes
