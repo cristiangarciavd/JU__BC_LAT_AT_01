444d90faacbe1a6a34e7677d8982f943bdb97245
NachoJala <111385139+NachoJala@users.noreply.github.com>
2022-09-15 08:38:04 -0700

    Update data_analize.py

--- django_project_api/project_api/api/data_analizer/data_analize.py
+++ django_project_api/project_api/api/data_analizer/data_analize.py
@@ -1,11 +1,10 @@
 import re
 from api.scraper.scraper import Scraper
 from api.parser.parser_factory import ParserFactory
-from api.db_manager.db_manager import DbManager
 
 class DataCollector():
     def __init__(self):
-        self.product_name = 'iphone X' #iphone X no funciona para amazon
+        self.product_name = 'camisas' #iphone X no funciona para amazon
         self.pages = []
         self.pages_id = {}
         self.__pages_products = [] # unsorted products list
@@ -23,9 +22,15 @@ class DataCollector():
 
     def __parse_data(self):
         for idx, page in enumerate(self.pages):
-            parse_type = self.__page_parser.parser(page + "parser")
-            self.__pages_products.append(parse_type.parse(self.__html_contents[idx][page])) # pages_products = [[{p1},{p2},{p3}],[{p1},{p2}...],...]
-
+                parse_type = self.__page_parser.parser(page + "parser")
+                self.__pages_products.append(parse_type.parse(self.__html_contents[idx][page])) # pages_products = [[{p1},{p2},{p3}],[{p1},{p2}...],...]
+        for single_page_products in self.__pages_products: #iterar hasta que sirva.
+            if len(single_page_products) == 0:
+                #logger recompilando datos
+                self.__html_contents.clear()
+                self.__pages_products.clear()
+                self.__html_contents = self.__web_scrapper.scrap(self.product_name)
+                self.__parse_data()
 
     def sort_by_price(self, reverse = False):
         self.__sorted_products.clear()
@@ -34,28 +39,8 @@ class DataCollector():
         return self.__sorted_products 
 
     def collect(self, product_name):
-        db_manager = DbManager()
-        search = db_manager.find_term(product_name)
-        if search:
-            search = db_manager.term_up_today(search)
-            if search:
-                self.__pages_products = db_manager.find_products(search)
-            else:
-                print('+++++++*********++++**++*+MUERTEEEEEEEE')
-                db_manager.delete_search(product_name)
-                self.__scrap_html_contents(product_name)
-                self.__parse_data()
-                self.sort_by_price()
-                products = self.top_x_products(5)
-                db_manager.create(products, product_name)
-        else:
-            self.__scrap_html_contents(product_name)
-            self.__parse_data()
-            self.sort_by_price()
-            products = self.top_x_products(5)
-            db_manager.create(products, product_name)
-        # for products in self.__pages_products: #To do: Must implement price validation in parsers files
-        #     self.price_validation(products)
+        self.__scrap_html_contents(product_name)
+        self.__parse_data()
 
     def top_x_products(self, x = 'all'):
         top_products = []
@@ -63,21 +48,24 @@ class DataCollector():
             top_page_products = []
             if x == 'all':
                 return self.__sorted_products
-            elif x > len(products) and x > 0: #Need to be upgraded
+            elif x > len(products) and x > 0:
                 print(self.pages[idx] + " store have " + str(len(products)) + " products to offer")
-                for idx in range(x):
-                    top_page_products.append(products[idx])
+                try:
+                    for idx in range(x):
+                        top_page_products.append(products[idx])
+                except:
+                    pass
             else:
                 for idx in range(x):
                     top_page_products.append(products[idx])
             top_products.append(top_page_products)
         return top_products
         
-
 # instance = DataCollector()
 # instance.collect('camisas para hombre')
+
 # instance.sort_by_price()
-# list = instance.top_x_products()
+# list = instance.top_x_products(5)
 # for page in list:
 #     for product in page:
 #         print(product['product'], ":", product['price'], product['origin'])
